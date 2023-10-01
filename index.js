f9457d1389b91abf9b22eac5a08f1e62c002d35b
Samuel Muigai <ndungu.muigai01@gmail.com>
2023-10-01 18:30:37 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+let names=[];
+
+function writeCards(array, event)
+{
+    let messages=[];
+    for (let i = 0; i < array.length; i++) 
+    {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(number)
+{
+    let i=0;
+    while (i <= number) 
+    {
+        console.log(i);
+        i++;     
+    }
+}
+
+names.push("James","John","Emanuel");
+
+console.log(countDown(11))
+console.log(writeCards(names,"birthday"));
